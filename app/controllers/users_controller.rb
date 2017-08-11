586294fb8cc4a793fc2c8c81a0424abe79913399
hideotty <n.n.hideo@gmail.com>
2017-08-11 20:05:41 +0900

    フラッシュメッセージの指定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ end
 
 def update
   if current_user.update(user_params)
-    redirect_to root_path
+    redirect_to root_path , notice: '更新しました'
   else
+    flash.now[:alart] ="更新に失敗しました"
     render :edit
   end
 end
