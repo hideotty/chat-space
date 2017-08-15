1e9809d57fe3c6e117f131e63e71c407b975b328
hideotty <n.n.hideo@gmail.com>
2017-08-15 16:40:32 +0900

    add asociation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-validates :name, presence: true
+  validates :name, presence: true
+  has_many :members
+  has_many :groups, through: :members
 end
