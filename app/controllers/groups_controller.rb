5dd463b994de6e09e9f3d0f85570213517148214
hideotty <n.n.hideo@gmail.com>
2017-08-16 19:44:51 +0900

    不要な記述を削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,4 @@
 class GroupsController < ApplicationController
-  # before_action :set_group, only:[:index]
   def index
     @groups = Group.all
 
@@ -31,8 +30,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name)
   end
-
-  # def set_group
-  #   @group = Group.find(params[:id])
-  # end
 end
