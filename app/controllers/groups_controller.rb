a53479ab70caf5384d3f5559a77a99c4b8e17607
hideotty <n.n.hideo@gmail.com>
2017-08-16 20:43:01 +0900

    グループ作成時のフラッシュメッセージ追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,8 +11,9 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path
+      redirect_to root_path, :notice : "グループを作成しました"
     else
+      flash.now[:alart] = "グループ名を入力してください"
       render :new
     end
   end
