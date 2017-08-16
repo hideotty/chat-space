9e5dba6a4209b23512b84865a98e18893271cb2d
hideotty <n.n.hideo@gmail.com>
2017-08-16 15:34:16 +0900

    new遷移先

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,10 +7,26 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
 
+      render :new
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
+  def update
+
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ end
 
 def update
   if current_user.update(user_params)
-    redirect_to root_path , notice: '更新しました'
+    redirect_to root_path
   else
     flash.now[:alart] ="更新に失敗しました"
     render :edit
@@ -13,8 +13,8 @@ def update
 end
 
 private
-def user_params
-  params.require(:user).permit(:name, :email)
-end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
 
