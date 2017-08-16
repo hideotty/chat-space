dc4a7995a43679958a75497099b936ee4d0cdc3a
hideotty <n.n.hideo@gmail.com>
2017-08-16 20:17:39 +0900

    gitignoreを反映させる

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:edit,:update,:new,:create]
-  resources :groups
-  devise_for :users
-  root 'groups#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
