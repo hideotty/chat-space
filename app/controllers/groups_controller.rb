14b15335082f6565f694f6b2924bf949750ccc95
hideotty <n.n.hideo@gmail.com>
2017-08-17 12:14:30 +0900

    フラッシメッセージの記述修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, :notice : "グループを作成しました"
+      redirect_to root_path, notice: "グループを作成しました"
     else
       flash.now[:alart] = "グループ名を入力してください"
       render :new
