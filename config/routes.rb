26de7f17ccf7f959fca022f235f05e9c2e832b8c
hideotty <n.n.hideo@gmail.com>
2017-08-17 17:12:32 +0900

    messageとgroupsをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit,:update,:new,:create]
-  resources :groups
+  resources :groups do
+    resources :messages
+  end
   devise_for :users
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
