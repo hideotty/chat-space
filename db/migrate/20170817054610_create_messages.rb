b938d38fe90b5b99fde26edd2b3c633b79f3e237
hideotty <n.n.hideo@gmail.com>
2017-08-22 13:31:03 +0900

    メッセージ投稿時のパス設定

--- db/migrate/20170817054610_create_messages.rb
+++ db/migrate/20170817054610_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.string :user_id
-      t.string :group_id
-      t.string :text
+      t.references :group, foreign_key: true, index: true
+      t.references :user, foreign_key: true, index: true
+
 
       t.timestamps
     end
--- db/migrate/20170819074843_create_members.rb
+++ db/migrate/20170819074843_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.string :user_id
-      t.string :group_id
+      t.references :group, foreign_key: true, index: true
+      t.references :user, foreign_key: true, index: true
 
       t.timestamps
     end
