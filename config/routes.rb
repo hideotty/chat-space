7c95c12814016d7571589c074fc4441caef0dad8
hideotty <n.n.hideo@gmail.com>
2017-08-22 14:14:55 +0900

    不要箇所の削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :groups do
     resources :messages
   end
-  # resources :messages
   devise_for :users
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
