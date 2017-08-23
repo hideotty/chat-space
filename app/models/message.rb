bb07bac6e04fd933633c3a1815a31ac82c5fe7e1
hideotty <n.n.hideo@gmail.com>
2017-08-23 16:09:43 +0900

    add uploader

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  mount_uploader :img, ImageUploader
+  validates :content_or_img, presence: true
+
+  private
+  def content_or_img
+    content.presence||img.presence
+  end
 end
