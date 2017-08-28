0188811f27700aabef37a75a2705dc801a0de947
hideotty <n.n.hideo@gmail.com>
2017-08-28 12:15:38 +0900

    password_confirmationカラムの追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
 
+  pass = Faker::Lorem.sentence
+
   factory :user do
     name { Faker::Name.name}
     email{ Faker::Internet.email }
-    password{ Faker::Lorem.sentence }
-
+    password{ pass }
+    password_confirmation { pass }
   end
 end
