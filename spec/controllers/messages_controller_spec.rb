68cec25a186a050c85f8d35647525a25a644815e
hideotty <n.n.hideo@gmail.com>
2017-08-31 16:16:42 +0900

    controller testの記述修正
    下記の点を修正
    ・ログインしているかつ、保存に成功した場合、意図した画面に遷移しているのか
    ・ログインしているが、保存に失敗した場合、メッセージの保存は行われなかったか

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -28,22 +28,34 @@ describe MessagesController do
   end
 
   describe "POST #create" do
+     let(:params) do
+      {
+        group_id: group, message: attributes_for(:message)
+      }
+      end
     context "user_signed_in and save a message" do
       before do
         user = create(:user)
         login_user(user)
       end
       it "is saved message" do
-        expect{ post :create, params: { group_id: group, message: attributes_for(:message) }}.to change(Message, :count).by(1)
+        expect{ post :create, params: params }.to change(Message, :count).by(1)
+      end
+      it "renders the :index  when failed to save a message" do
+        post :create, params: params
+          expect(response).to redirect_to group_messages_path
+      end
+      it "is not saved a message when failed to save a message" do
+        expect{ post :create, params:{ group_id: group, message: attributes_for(:message, content: nil, img: nil) }}.to change(Message, :count).by(0)
       end
       it "redirect to index when failed to save a message" do
-        post :create, params: { group_id: group, message: attributes_for(:message)}
-          expect(response).to redirect_to group_messages_path
+        post :create, params: params
+        expect(response).to redirect_to group_messages_path
       end
     end
     context "not user_signed_in" do
       it "redirect to sign_up template" do
-        post :create, params:{ group_id: group, message: attributes_for(:message)}
+        post :create, params: params
         expect(response).to redirect_to new_user_session_path
       end
     end
