25788eded148916b6f94710a7b5e94cfff3d2b6f
hideotty <n.n.hideo@gmail.com>
2017-09-07 12:10:27 +0900

    routeの記述場所修正
    メンターの指摘でdeviceに関するroutesの記述場所を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:edit,:update]
+  devise_for :users
+  resources :users, only: [:edit,:update,:index]
   resources :groups do
     resources :messages
   end
-  devise_for :users
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
