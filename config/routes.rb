e151e695f79768a823b6a4547baf739b94078e8c
hideotty <n.n.hideo@gmail.com>
2017-09-07 15:51:52 +0900

    routes記述修正
    resoucesで作成した不要なアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit,:update,:index]
-  resources :groups do
-    resources :messages
-  end
-
   root 'groups#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit,:update,:index]
+  resources :groups, only: [:edit,:update,:new,:create] do
+    resources :messages, only: [:index, :create]
+  end
 end
